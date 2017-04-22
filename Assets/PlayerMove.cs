b9c90cd6ed02bb64367019bc48248fe664da04aa
KristofferLH <kristoffer_lh@hotmail.com>
2017-04-22 10:58:13 +0200

    playerMove+Look

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour {
 
     Rigidbody rig;
-	// Use this for initialization
-	void Start () {
-		
+    Transform cam;
+    public float Speed;
+    // Use this for initialization
+    void Start () {
+        cam = transform.GetChild(0);
+        rig = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
-	}
+        rig.velocity += transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
+        rig.velocity += transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * Speed;
+        rig.drag = rig.velocity.magnitude;
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+            rig.velocity = Vector3.zero;
+        
+    }
 }
