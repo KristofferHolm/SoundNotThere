3487df2605e91c334f52ef1f2769bc99ba910e86
KristofferLH <kristoffer_lh@hotmail.com>
2017-04-23 03:36:04 +0200

    soundmanager added

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
 
+    public float minWait, maxWait;
     List<GameObject> ListOfGO;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         ListOfGO = new List<GameObject>();
         ListOfGO.AddRange(GameObject.FindGameObjectsWithTag("Papfigur"));
-	}
-	
+    }
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    public void RemoveMeFromList(GameObject removeGO)
+    {
+        ListOfGO.Remove(removeGO);
+        if (ListOfGO.Count == 0)
+            EndGameScenario();
+    }
+    public void StartGame()
+    {
+        AkSoundEngine.PostEvent("BGM_Play", gameObject);
+        StartCoroutine(StartRandomSound());
+    }
+
+    private IEnumerator StartRandomSound()
+    {
+        yield return new WaitForSeconds(Random.Range(minWait, maxWait));
+        if(ListOfGO.Count == 0)
+            yield return null;
+        else
+        {
+            GameObject temp = ListOfGO[0];
+            AkSoundEngine.PostEvent(temp.name, temp);
+            ListOfGO.Remove(ListOfGO[0]);
+            ListOfGO.Add(temp);
+            StartCoroutine(StartRandomSound());
+            yield return null;
+        }
+    }
+
+    public void EndGameScenario()
+    {
+        print("the end is not there!");
+    }
+
 }
