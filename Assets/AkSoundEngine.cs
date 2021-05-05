f830f5e873ffbf4cb522c04ecfe7bbdd1435e455
Kristoffer <kristoffer_lh@hotmail.com>
2021-05-05 21:08:34 +0200

    update Unity to 2021.1.4f1 + Oculus Integration + Wwise removed!

---
+++ Assets/AkSoundEngine.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AkSoundEngine
+{
+    public static void PostEvent(string sound, GameObject obj)
+    {
+
+    }
+}
