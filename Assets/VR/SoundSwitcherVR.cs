4265d16c3524431cdb3a134953fbf152da5348b6
Kristoffer <kristoffer_lh@hotmail.com>
2021-05-20 14:18:40 +0200

    man kan spille det igennem nu, men: controls skal laves om på: - man skal kunne teleporte på venstre hånd - man skal kunne bytte lyd på højre trigger Lytteren er i kameraet, men sidder et sted i midten af køkkenet eller der hvor man spawner med retning mod spawn. slutning skal laves så den passer bedre til VR, man får canvas i højre øje kun. lydholderne skal IKKE spille en lyd efter swap. Swap ser dumt ud, men det går nok Der mangler feedback på at det er ens lydhorn som spiller lyden (jeg tænker bare emission og shake er fint)

---
+++ Assets/VR/SoundSwitcherVR.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSwitcherVR : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    public GameObject lazer;
+    private MeshRenderer lazerMesh;
+    public float Range;
+    public string SoundBit;
+    public SoundBoard.Sound Sound = SoundBoard.Sound.Null;
+    public GameObject AudioSphere;
+    public AnimationCurve AC;
+    SoundBoard SB;
+    float RightClickCD = 0;
+    LayerMask layerMask;
+    void Start()
+    {
+        lazerMesh = lazer.GetComponent<MeshRenderer>();
+        layerMask = LayerMask.NameToLayer("Interactable");
+        layerMask = ~layerMask;
+        SB = GameObject.Find("GameManager").GetComponent<SoundBoard>();
+    }
+    private void OnValidate()
+    {
+        if (Sound == SoundBoard.Sound.Null)
+        {
+            System.Enum.TryParse(SoundBit, out SoundBoard.Sound sEnum);
+            Sound = sEnum;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ray.direction = lazer.transform.up * Range;
+        ray.origin = lazer.transform.position;
+        lazerMesh.enabled = OVRInput.Get(OVRInput.Button.Any);
+        if (Physics.Raycast(ray, out hit, Range, layerMask))
+        {
+            if (hit.collider.tag == "PapFigur")
+            {
+                hit.transform.GetComponent<SoundHolder>().LookedAt();
+                if (OVRInput.GetUp(OVRInput.Button.One) || OVRInput.GetUp(OVRInput.Button.Two))
+                    PlaySound(hit.transform.gameObject);
+                if (OVRInput.GetUp(OVRInput.RawButton.LIndexTrigger) || OVRInput.GetUp(OVRInput.RawButton.LHandTrigger))
+                    SwitchSound(hit.collider.GetComponent<SoundHolder>());
+            }
+        }
+        else if (OVRInput.GetUp(OVRInput.RawButton.LIndexTrigger) || OVRInput.GetUp(OVRInput.RawButton.LHandTrigger))
+        {
+            ListenInHand();
+        }
+        if (RightClickCD > 0)
+            RightClickCD -= Time.deltaTime;
+    }
+
+    private void ListenInHand()
+    {
+        if (RightClickCD > 0)
+            return;
+        else
+            RightClickCD = SB.GetsSoundLength(Sound);
+        AkSoundEngine.PostEvent(Sound, gameObject);
+        StartCoroutine(ChangeHorn(RightClickCD));
+    }
+    IEnumerator ChangeHorn(float time)
+    {
+        //change graphic
+        yield return new WaitForSeconds(time);
+        //change back
+        yield return null;
+    }
+    private void SwitchSound(SoundHolder SH)
+    {
+        if (RightClickCD > 0 || SH.Completed)
+            return;
+        else
+            RightClickCD = 2.0f;
+        StartCoroutine(ChangeHorn(2.0f));
+        AkSoundEngine.PostEvent(SoundBoard.Sound.Swap, gameObject);
+        //sphere from papfigur
+        StartCoroutine(AnimateAudioSphere(SH.SoundEnum, hit.transform.position, transform.position + transform.forward * 0.05f, -transform.right));
+        //sphere from player
+        StartCoroutine(AnimateAudioSphere(Sound, transform.position + transform.forward * 0.05f, hit.transform.position, transform.right));
+        if (SH.Completed)
+            return;
+        var temp = Sound;
+        Sound = SH.SoundEnum;
+        SH.SoundEnum = temp;
+        if (SH.SoundEnum.ToString() == SH.name)
+            SH.Complete();
+    }
+
+    private IEnumerator AnimateAudioSphere(SoundBoard.Sound sound, Vector3 from, Vector3 to, Vector3 turn)
+    {
+        float t = 0;
+        GameObject sphere = Instantiate<GameObject>(AudioSphere);
+        AkSoundEngine.PostEvent(sound, sphere);
+        sphere.transform.position = transform.position;
+        while (t < 1f)
+        {
+
+            t += Time.deltaTime / 2.0f; // så det tager 2 sekunder
+            sphere.transform.position = Vector3.Lerp(from, to, t) + turn * AC.Evaluate(t); ;
+            yield return null;
+        }
+        Destroy(sphere);
+        yield return null;
+    }
+    private void PlaySound(GameObject go)
+    {
+        if (RightClickCD > 0)
+            return;
+        go.GetComponent<SoundHolder>().PlaySound();
+    }
+
+}
